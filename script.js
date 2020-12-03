cd9ec42fe1ce1c747050cdddc11b01f148061396
Faith Sansom <73004209+Bre98@users.noreply.github.com>
2020-12-03 05:52:07 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+    document.getElementById('demo').style.display='none';
+    const cardContent = document.querySelector('.flip');
+    
+    
+    
+    
+    
+    
+    function chkyr(){
+    var chk = document.getElementById('yr').value;
+    var result = document.getElementById('demo');
+   
+    
+    if((chk % 400==0) || ((chk %100 !=0) && (chk % 4==0)))
+    {
+        result.style.display = "block";
+        document.getElementById('demo').innerHTML = 'Result: ' + chk + ' is a leap year!';
+        
+    }
+    else
+    {
+        result.style.display = "block";
+        document.getElementById('demo').innerHTML = 'Result: ' + chk + ' is not a leap year.';
+        
+    }
+    
+}
+
+
+
+
+
