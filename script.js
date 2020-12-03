ea0088eed5118b65b8799c92b7a9986a0e661715
Bre98 <73004209+Bre98@users.noreply.github.com>
2020-12-03 08:39:58 -0600

    More positioning

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 
     document.getElementById('demo').style.display='none';
     
+    
+    
     function ClearFields() {
 
         document.getElementById("yr").value = "";
-
+        
    }
-    
-    
-    
-    
+      
     function chkyr(){
     var chk = document.getElementById('yr').value;
     var result = document.getElementById('demo');
-   
     
-    if((chk % 400==0) || ((chk %100 !=0) && (chk % 4==0)))
+  if((chk % 400==0) || ((chk %100 !=0) && (chk % 4==0)))
     {
-        result.style.display = "block";
         document.getElementById('demo').innerHTML = chk + ' is a leap year!';
+        result.style.display = "block";
+        ClearFields();
         
     }
     else
     {
-        result.style.display = "block";
         document.getElementById('demo').innerHTML = chk + ' is not a leap year.';
+        result.style.display = "block";
+        ClearFields();
         
         
     }
-   ClearFields(); 
+    
+   
     
 }
 
